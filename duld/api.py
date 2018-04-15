fc22ceb97e71e8773ff0dccf771360ef4fa79c7d
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-04-16 07:20:27 +0800

    rewrite api

--- duld/api.py
+++ duld/api.py
@@ -1,27 +1,30 @@
+import asyncio
 import json
 
-from tornado import web as tw, ioloop as ti
+from aiohttp import web as aw
 
 from . import torrent
 
 
-class TorrentsHandler(tw.RequestHandler):
+class TorrentsHandler(aw.View):
 
-    def post(self):
+    async def post(self):
         torrents = torrent.get_completed()
-        uploader = self.settings['uploader']
-        loop = ti.IOLoop.current()
+        uploader = self.request.app['uploader']
+        loop = asyncio.get_event_loop()
         for t in torrents:
-            loop.add_callback(torrent.upload_torrent, uploader, t.id)
+            f = torrent.upload_torrent(uploader, t.id)
+            loop.create_task(f)
         result = json.dumps([_.id for _ in torrents])
-        self.write(result)
+        return aw.Response(text=result)
 
-    def put(self, torrent_id):
+    async def put(self):
+        torrent_id = self.request.match_info['torrent_id']
         if not torrent_id:
-            self.set_status(400)
-            return
+            return aw.Response(status=400)
 
-        uploader = self.settings['uploader']
-        loop = ti.IOLoop.current()
-        loop.add_callback(torrent.upload_torrent, uploader, torrent_id)
-        self.set_status(204)
+        uploader = self.request.app['uploader']
+        loop = asyncio.get_event_loop()
+        f = torrent.upload_torrent(uploader, torrent_id)
+        loop.create_task(f)
+        return aw.Response(status=204)
