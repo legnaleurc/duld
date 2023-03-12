ded421abf98601c789d54a27e72eb9c6b526953c
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-03-13 02:50:58 +0900

    add dataclasses

--- duld/settings.py
+++ duld/settings.py
@@ -1,6 +1,33 @@
+from dataclasses import dataclass
 import sys
 
 import yaml
+
+
+@dataclass
+class DiskSpaceData:
+    safe: int
+    danger: int
+
+
+@dataclass
+class TransmissionData:
+    host: str
+    port: int
+    username: str | None
+    password: str | None
+
+
+@dataclass
+class Data:
+    port: int
+    upload_to: str
+    log_path: str | None
+    exclude_pattern: list[str] | None
+    exclude_url: str | None
+    reserved_space_in_gb: DiskSpaceData | None
+    transmission: TransmissionData | None
+    hah_path: str | None
 
 
 class Settings(object):
