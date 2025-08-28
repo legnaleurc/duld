38f8f834ad717d219d48a04da5580e0a1edbde08
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-08-29 01:37:02 +0900

    feat: migrate to rootless podman

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,3 @@
 #! /bin/sh
 
-if [ -z "$DULD_UID" ] || [ -z "$DULD_GID" ] ; then
-    exit 1
-fi
-
-exec setpriv \
-    --reuid="$DULD_UID" \
-    --regid="$DULD_GID" \
-    --groups="$DULD_GID" \
-    poetry run -- \
-    python3 -m duld --settings=/mnt/duld.yaml
+exec poetry run -- python3 -m duld --settings=/mnt/duld.yaml
